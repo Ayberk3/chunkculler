da177d82ccccfc1dc186988491d3d6f479ae53c9
Ayberk3 <142735728+Ayberk3@users.noreply.github.com>
2026-08-23 15:24:54 +0300

    SubChunkCuller v1.0.0 - Unified SubChunk & Zero-Leak Anti-ESP: src/main/java/dev/ayberk/subchunkculler/EntityPacketListener.java

---
+++ src/main/java/dev/ayberk/subchunkculler/EntityPacketListener.java
@@ -0,0 +1,133 @@
+package dev.ayberk.subchunkculler;
+
+import com.github.retrooper.packetevents.event.PacketListenerAbstract;
+import com.github.retrooper.packetevents.event.PacketListenerPriority;
+import com.github.retrooper.packetevents.event.PacketSendEvent;
+import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import com.github.retrooper.packetevents.wrapper.play.server.*;
+import org.bukkit.entity.Player;
+
+import java.util.UUID;
+
+public final class EntityPacketListener extends PacketListenerAbstract {
+
+    private final Main plugin;
+
+    public EntityPacketListener(Main plugin) {
+        super(PacketListenerPriority.NORMAL);
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void onPacketSend(PacketSendEvent event) {
+        if (!plugin.getConfigManager().isEntityCullerEnabled()) {
+            return;
+        }
+
+        if (!(event.getPlayer() instanceof Player player)) {
+            return;
+        }
+
+        if (player.hasPermission("subchunkculler.bypass")) {
+            return;
+        }
+
+        UUID viewerUUID = player.getUniqueId();
+        var packetType = event.getPacketType();
+        EntityTrackingManager tracking = plugin.getEntityTrackingManager();
+
+        if (packetType == PacketType.Play.Server.SPAWN_ENTITY) {
+            WrapperPlayServerSpawnEntity packet = new WrapperPlayServerSpawnEntity(event);
+            int entityId = packet.getEntityId();
+            double entityY = packet.getPosition().getY();
+            double viewerY = player.getLocation().getY();
+
+            if ((viewerY - entityY) >= plugin.getConfigManager().getHideDistanceY()) {
+                tracking.hideEntity(player, player.getWorld().getEntities().stream()
+                        .filter(e -> e.getEntityId() == entityId)
+                        .findFirst().orElse(null));
+                event.setCancelled(true);
+                return;
+            }
+        }
+
+        if (packetType == PacketType.Play.Server.ENTITY_RELATIVE_MOVE) {
+            WrapperPlayServerEntityRelativeMove packet = new WrapperPlayServerEntityRelativeMove(event);
+            if (tracking.isEntityHidden(viewerUUID, packet.getEntityId())) {
+                event.setCancelled(true);
+            }
+        } else if (packetType == PacketType.Play.Server.ENTITY_RELATIVE_MOVE_AND_ROTATION) {
+            WrapperPlayServerEntityRelativeMoveAndRotation packet = new WrapperPlayServerEntityRelativeMoveAndRotation(event);
+            if (tracking.isEntityHidden(viewerUUID, packet.getEntityId())) {
+                event.setCancelled(true);
+            }
+        } else if (packetType == PacketType.Play.Server.ENTITY_ROTATION) {
+            WrapperPlayServerEntityRotation packet = new WrapperPlayServerEntityRotation(event);
+            if (tracking.isEntityHidden(viewerUUID, packet.getEntityId())) {
+                event.setCancelled(true);
+            }
+        } else if (packetType == PacketType.Play.Server.ENTITY_TELEPORT) {
+            WrapperPlayServerEntityTeleport packet = new WrapperPlayServerEntityTeleport(event);
+            if (tracking.isEntityHidden(viewerUUID, packet.getEntityId())) {
+                event.setCancelled(true);
+            }
+        } else if (packetType == PacketType.Play.Server.ENTITY_HEAD_LOOK) {
+            WrapperPlayServerEntityHeadLook packet = new WrapperPlayServerEntityHeadLook(event);
+            if (tracking.isEntityHidden(viewerUUID, packet.getEntityId())) {
+                event.setCancelled(true);
+            }
+        } else if (packetType == PacketType.Play.Server.ENTITY_METADATA) {
+            WrapperPlayServerEntityMetadata packet = new WrapperPlayServerEntityMetadata(event);
+            if (tracking.isEntityHidden(viewerUUID, packet.getEntityId())) {
+                event.setCancelled(true);
+            }
+        } else if (packetType == PacketType.Play.Server.ENTITY_EQUIPMENT) {
+            WrapperPlayServerEntityEquipment packet = new WrapperPlayServerEntityEquipment(event);
+            if (tracking.isEntityHidden(viewerUUID, packet.getEntityId())) {
+                event.setCancelled(true);
+            }
+        } else if (packetType == PacketType.Play.Server.ENTITY_VELOCITY) {
+            WrapperPlayServerEntityVelocity packet = new WrapperPlayServerEntityVelocity(event);
+            if (tracking.isEntityHidden(viewerUUID, packet.getEntityId())) {
+                event.setCancelled(true);
+            }
+        } else if (packetType == PacketType.Play.Server.ENTITY_ANIMATION) {
+            WrapperPlayServerEntityAnimation packet = new WrapperPlayServerEntityAnimation(event);
+            if (tracking.isEntityHidden(viewerUUID, packet.getEntityId())) {
+                event.setCancelled(true);
+            }
+        } else if (packetType == PacketType.Play.Server.ENTITY_STATUS) {
+            WrapperPlayServerEntityStatus packet = new WrapperPlayServerEntityStatus(event);
+            if (tracking.isEntityHidden(viewerUUID, packet.getEntityId())) {
+                event.setCancelled(true);
+            }
+        } else if (packetType == PacketType.Play.Server.UPDATE_ATTRIBUTES) {
+            WrapperPlayServerUpdateAttributes packet = new WrapperPlayServerUpdateAttributes(event);
+            if (tracking.isEntityHidden(viewerUUID, packet.getEntityId())) {
+                event.setCancelled(true);
+            }
+        } else if (packetType == PacketType.Play.Server.SET_PASSENGERS) {
+            WrapperPlayServerSetPassengers packet = new WrapperPlayServerSetPassengers(event);
+            if (tracking.isEntityHidden(viewerUUID, packet.getEntityId())) {
+                event.setCancelled(true);
+            }
+        } else if (plugin.getConfigManager().isDampenUndergroundSounds() &&
+                (packetType == PacketType.Play.Server.SOUND_EFFECT || packetType == PacketType.Play.Server.NAMED_SOUND_EFFECT)) {
+            if (packetType == PacketType.Play.Server.SOUND_EFFECT) {
+                WrapperPlayServerSoundEffect soundPacket = new WrapperPlayServerSoundEffect(event);
+                double soundY = soundPacket.getFixedPosition().getY();
+                double viewerY = player.getLocation().getY();
+                if ((viewerY - soundY) >= plugin.getConfigManager().getHideDistanceY()) {
+                    event.setCancelled(true);
+                }
+            } else {
+                WrapperPlayServerNamedSoundEffect soundPacket = new WrapperPlayServerNamedSoundEffect(event);
+                double soundY = soundPacket.getFixedPosition().getY();
+                double viewerY = player.getLocation().getY();
+                if ((viewerY - soundY) >= plugin.getConfigManager().getHideDistanceY()) {
+                    event.setCancelled(true);
+                }
+            }
+        }
+    }
+}
